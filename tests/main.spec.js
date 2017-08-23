8e12f8bbdf8262a7664d2866bbe532c6e3d5d36e
William Goulart <williamgoulart1@hotmail.com>
2017-08-23 03:51:09 -0300

    Update package.json & Add Example test

---
+++ tests/main.spec.js
@@ -0,0 +1,18 @@
+/*
+* Describe: Using to Class, Files or Method to test
+* Context: Using describe Methods to test
+*/
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      it('should happen sum 2 numbers', () => {
+      });
+    });
+
+    context('Case 2', () => {
+    });
+  });
+
+  describe('Method A', () => {
+  });
+});

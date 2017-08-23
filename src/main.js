f69c44788f55cd634db1503c23f7078f1f811713
William Goulart <williamgoulart1@hotmail.com>
2017-08-23 05:27:58 -0300

    Add tests to Calc

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 module.exports = {
-  sum: function() {},
-  sub: function() {},
-  div: function() {},
-  mult: function() {}
+  sum: (firstNumber, secondNumber) => firstNumber + secondNumber,
+  sub: (firstNumber, secondNumber) => firstNumber - secondNumber,
+  mult: (firstNumber, secondNumber) => firstNumber * secondNumber,
+  div: (firstNumber, secondNumber) => {
+    return secondNumber === 0 ? `Não é possivel divisão por zero!` : firstNumber / secondNumber
+  }
 }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,12 +1,11 @@
 var expect = require('chai').expect;
-var calc =  require('../src/main.js');
+var calc = require('../src/main.js');
 
-describe('Calc', function() {
+describe('Calc', function () {
   // Smoke test
   describe('Smoke tests', () => {
     it('should exist the calc lib', () => {
       expect(calc).to.exist;
-
     });
 
     it('should exist the method `sum`', () => {
@@ -28,6 +27,38 @@ describe('Calc', function() {
       expect(calc.div).to.exist;
       expect(calc.div).to.be.a('function');
     });
-
   })
+
+  describe('Sum 2 numbers', () => {
+    it('should return 4 when `sum(2,2)`', () => {
+      expect(calc.sum(2, 2)).to.be.equals(4);
+    });
+  });
+
+  describe('Sub 2 numbers', () => {
+    it('should return 4 when `sub(6,2)`', () => {
+      expect(calc.sub(6, 2)).to.be.equals(4);
+    });
+
+    it('should return -4 when `sub(6,10)`', () => {
+      expect(calc.sub(6, 10)).to.be.equals(-4);
+    });
+  });
+
+  describe('Multiply 2 numbers', () => {
+    it('should return 4 when `mult(2,2)`', () => {
+      expect(calc.mult(2, 2)).to.be.equals(4);
+    });
+  });
+
+  describe('Divided 2 numbers', () => {
+    it('should return 5 when `div(10,2)`', () => {
+      expect(calc.div(10, 2)).to.be.equals(5);
+    });
+
+    it('should return `Não é possivel divisão por zero!`', () => {
+      expect(calc.div(4, 0)).to.be.equals('Não é possivel divisão por zero!');
+    });
+  });
+
 });

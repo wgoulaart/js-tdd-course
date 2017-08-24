a8b19c0f327958f957c926c5c4f055b425a500fe
William Goulart <williamgoulart1@hotmail.com>
2017-08-23 23:33:37 -0300

    Fix Lint & Code FizzBuzz

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 const FizzBuzz = (number) => {
-  if(number % 3 === 0 && number % 5 === 0) return 'FizzBuzz'
-  if(number % 3 === 0) return 'Fizz';
-  if(number % 5 === 0) return 'Buzz';
+  if (number === 0) return 0;
+  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+  if (number % 3 === 0) return 'Fizz';
+  if (number % 5 === 0) return 'Buzz';
+
   return number;
-}
+};
 
 export default FizzBuzz;
